80b10f4f12201d2e531a46ac8ef523ce46ba4026
Bruno <bgoldoni@valid.com.br>
2020-10-31 14:11:25 -0300

    Backend - methods http

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const { response } = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello GoStack..." });
+app.get("/projects", (request, response) => {
+  return response.json(["Project 1", "Project 2"]);
+});
+
+app.post("/projects", (request, response) => {
+  return response.json(["Project 1", "Project 2", "Project 3"]);
+});
+
+app.put("/projcts/:id", (request, response) => {
+  return response.json(["Project 4", "Project 2", "Project 3"]);
+});
+
+app.delete("/projects", (request, resposne) => {
+  return response.json(["Project 2", "Project 3"]);
 });
 
 app.listen(3333, () => {
