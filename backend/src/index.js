6334d2f5c722d262d38451295475680aa549da16
Bruno <bgoldoni@valid.com.br>
2020-11-01 18:17:23 -0300

    Backend - types of params

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,15 +3,31 @@ const { response } = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/projects", (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json(["Project 1", "Project 2"]);
 });
 
 app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json(["Project 1", "Project 2", "Project 3"]);
 });
 
 app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
   return response.json(["Project 4", "Project 2", "Project 3"]);
 });
 
