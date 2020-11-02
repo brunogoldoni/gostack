8a18582037c99e8b7dd22a6f5b6ad1e2ef26f33b
Bruno <bgoldoni@valid.com.br>
2020-11-02 19:37:36 -0300

    Mobile - integration with api

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,14 +1,40 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
+
+import api from "./services/api";
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      console.log(response.data);
+
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>GoStack | React Native</Text>
-        <Text style={styles.title}>Bruno Goldoni</Text>
-      </View>
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project} key={project.title}>
+              {project.title}
+            </Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -16,14 +42,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#7159c1",
-    alignItems: "center",
     flex: 1,
-    justifyContent: "center",
+    padding: 20,
   },
 
-  title: {
+  project: {
     color: "#FFF",
     fontSize: 20,
-    fontWeight: "bold",
   },
 });
