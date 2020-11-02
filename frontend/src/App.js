0f807e5ef7adc189aee5a8c30509de64bdb6b4ea
Bruno <bgoldoni@valid.com.br>
2020-11-02 13:49:19 -0300

    Frontend - props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,19 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header />
+      <Header title="Home">
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   );
 }
