cb3c24875a6f28781a49d76b8a1597f771407cd3
Bruno <bgoldoni@valid.com.br>
2020-11-02 20:32:54 -0300

    Typescript - typing objects and vectors

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: "brgodloni@gmail.com",
+    passowrd: "123456",
+    techs: [
+      "Node.js",
+      "ReactJS",
+      "React Native",
+      { title: "JavaScript", experience: 100 },
+    ],
+  });
+
   return response.json({ message: "Hello World" });
 }
