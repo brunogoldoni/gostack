e9838bac33c20cfd6ca4b4b7010b37a80bae8206
Bruno <bgoldoni@valid.com.br>
2020-11-02 18:21:59 -0300

    Difference between React Native and ReactJS

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Bruno Goldoni - React Native</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <View style={styles.container}>
+        <Text style={styles.title}>GoStack | React Native</Text>
+        <Text style={styles.title}>Bruno Goldoni</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#7159c1",
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#FFF",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
