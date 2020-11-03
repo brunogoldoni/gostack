496d3af9e4a96d54793b2740cb323c151ba276f2
Bruno <bgoldoni@valid.com.br>
2020-11-03 20:24:25 -0300

    NodeJS - Debug NodeJS

--- first-project-node/src/server.ts
+++ first-project-node/src/server.ts
@@ -1,13 +1,16 @@
-import express from "express";
+import express from 'express';
 
-import routes from "./routes";
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) =>
-  response.json({ message: "Hello GoStack" })
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello GoStack' }),
 );
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333!");
+  console.log('Server started on port 3333!');
 });
