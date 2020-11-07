05fa8f24ceb49391eb4b1d20e101a96150d50142
Bruno <bgoldoni@valid.com.br>
2020-11-07 09:29:46 -0300

    Web - Properties

--- web/src/App.js
+++ web/src/App.js
@@ -5,8 +5,19 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Home">
+        <ul>
+          <li>Starter</li>
+          <li>GoStack</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Backend</li>
+          <li>Users</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   );
 }
