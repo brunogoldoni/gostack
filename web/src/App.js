7527672c78235fb5960b6ffac9e62b3d0071f115
Bruno <bgoldoni@valid.com.br>
2020-11-07 15:33:04 -0300

    Web - listing API projects

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./App.css";
-import backgroundImage from "./assets/headphone.jpeg";
 
 import Header from "./components/Header";
 
 function App() {
-  const [projects, setProjects] = useState([
-    "Desenvolvimento de app",
-    "Front-end Web",
-  ]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
     setProjects([...projects, `New project - ${Date.now()}`]);
@@ -21,11 +25,9 @@ function App() {
     <>
       <Header title="Projects" />
 
-      <img width={150} src={backgroundImage} alt="" />
-
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
