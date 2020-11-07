2bf4f1a3ebdbdd5a1b4f2f7b5714a07ff459e590
Bruno <bgoldoni@valid.com.br>
2020-11-07 15:47:07 -0300

    Web - Registering projects

--- web/src/App.js
+++ web/src/App.js
@@ -15,10 +15,15 @@ function App() {
     });
   }, []);
 
-  function handleAddProject() {
-    setProjects([...projects, `New project - ${Date.now()}`]);
+  async function handleAddProject() {
+    const response = await api.post("projects", {
+      title: `New project ${Date.now()}`,
+      owner: "Bruno Goldoni",
+    });
 
-    console.log(projects);
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
