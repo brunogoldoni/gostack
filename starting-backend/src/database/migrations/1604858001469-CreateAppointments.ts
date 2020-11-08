9d17d2141cb29c57d0dd619ed590a8b7a707a098
Bruno <bgoldoni@valid.com.br>
2020-11-08 16:08:41 -0300

    Repository TypeORM

--- starting-backend/src/database/migrations/1604858001469-CreateAppointments.ts
+++ starting-backend/src/database/migrations/1604858001469-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1604858001469
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- starting-backend/src/server.ts
+++ starting-backend/src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import 'reflect-metadata';
 
+import express from 'express';
 import routes from './routes';
 
 import './database';
