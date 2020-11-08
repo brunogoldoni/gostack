f071f96c38c1740bce299e5f24663dfdf4d2c5d8
Bruno <bgoldoni@valid.com.br>
2020-11-08 17:14:33 -0300

    Relationships in the models

--- starting-backend/src/database/migrations/1604858001469-CreateAppointments.ts
+++ starting-backend/src/database/migrations/1604858001469-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1604858001469
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
