7ef1cfc67c3698e8b3b07c412003951fdba7e834
Bruno <bgoldoni@valid.com.br>
2020-11-09 20:11:38 -0300

    Upload files

--- starting-backend/src/routes/users.routes.ts
+++ starting-backend/src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -17,12 +22,22 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    // delete user.password;
+    delete user.password;
 
     return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
