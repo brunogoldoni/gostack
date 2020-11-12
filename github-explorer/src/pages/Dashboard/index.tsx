e96738fa084ac119bd8a1bfb01a96a4e41b2c74c
Bruno <bgoldoni@valid.com.br>
2020-11-12 17:38:55 -0300

    Styling dashboard

--- github-explorer/src/pages/Dashboard/index.tsx
+++ github-explorer/src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/51120700?s=400&u=8a94f2305aaab882f3b9c4a2e1d2fa65fb45ed77&v=4"
+            alt="Bruno Goldoni"
+          />
+          <div>
+            <strong>brunogoldoni/gostack</strong>
+            <p>Easy peasy highly scalable ReactJS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/51120700?s=400&u=8a94f2305aaab882f3b9c4a2e1d2fa65fb45ed77&v=4"
+            alt="Bruno Goldoni"
+          />
+          <div>
+            <strong>brunogoldoni/gostack</strong>
+            <p>Easy peasy highly scalable ReactJS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/51120700?s=400&u=8a94f2305aaab882f3b9c4a2e1d2fa65fb45ed77&v=4"
+            alt="Bruno Goldoni"
+          />
+          <div>
+            <strong>brunogoldoni/gostack</strong>
+            <p>Easy peasy highly scalable ReactJS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- github-explorer/src/pages/Dashboard/styles.ts
+++ github-explorer/src/pages/Dashboard/styles.ts
@@ -1,6 +1,95 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   color: #3a3a3a;
   font-size: 48px;
+  line-height: 56px;
+  margin-top: 80px;
+  max-width: 450px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700;
+  display: flex;
+
+  input {
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    background: #04d361;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    font-weight: bold;
+    height: 70px;
+    transition: background-color 0.3s;
+    width: 230px;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    display: block;
+    display: flex;
+    padding: 24px;
+    text-decoration: none;
+    transition: transform 0.2s;
+    width: 100%;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      border-radius: 50%;
+      height: 50px;
+      width: 54px;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        color: #3d3d4d;
+        font-size: 20px;
+      }
+
+      p {
+        color: #a8a8b3;
+        font-size: 18px;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      color: #cbcbd6;
+      margin-left: auto;
+    }
+  }
 `;
