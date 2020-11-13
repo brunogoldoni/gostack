f3449805d0c6c915bd6c28598f0a2c5fdfc023b9
Bruno <bgoldoni@valid.com.br>
2020-11-13 17:26:16 -0300

    Global styles

--- web-gobarber/src/App.tsx
+++ web-gobarber/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>Hello GoBarber...</h1>;
+  return (
+    <>
+      <h1>Hello GoBarber...</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
