1e851acbb52d21138b7fbf0e55fb87121c16c2c0
Bruno <bgoldoni@valid.com.br>
2020-11-19 20:09:41 -0300

    Authenticated routes

--- web-gobarber/src/routes/index.tsx
+++ web-gobarber/src/routes/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
 
-import { Switch, Route } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SingUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/Dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
