43e8be340ff078e7fd9bcf660f2de66d50d84ea2
Bruno <bgoldoni@valid.com.br>
2020-11-20 17:46:50 -0300

    Structure and patterns - Config StatusBar

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar, View, Text } from 'react-native';
 
 const App: React.FC = () => (
-  <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-    <Text>App - GoBarber</Text>
-  </View>
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Text>App - GoBarber</Text>
+    </View>
+  </>
 );
 
 export default App;
