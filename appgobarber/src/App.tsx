3f9a1519389ab452c157cb6f85389d73eafabd13
Bruno <bgoldoni@valid.com.br>
2020-11-20 17:30:03 -0300

    Strcture and patterns - Config. Sctructure

---
+++ appgobarber/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: React.FC = () =>
+  <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+    <Text>App - GoBarber</Text>
+  </View>
+
+export default App;
